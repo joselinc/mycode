00498bc659f34e775977ae2e7e423ac67b8cb88c
joselinc <joselinc@users.noreply.github.com>
2022-06-13 21:17:15 +0000

    Learning about input function

---
+++ input.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+pie = input("What's your name \n")
+print(f"Your name is", pie)
---
+++ lab_input/iptaker.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""print() - display data to std out"""
+
+def main():
+
+    user_input = input("Please enter an IPV4 address\n")
+
+    print("Your IPV4 address you input is", user_input)
+
+    print("\nThank you! \n")
+
+    user_name = input("Now, what is your name\n")
+
+    print("Your name is", user_name)
+    
+
+main()
