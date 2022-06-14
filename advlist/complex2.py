e663d587fd33cafe39f80f6b2fc2653917807961
joselinc <joselinc@users.noreply.github.com>
2022-06-14 14:37:12 +0000

    About complex lists

---
+++ advlist/complex2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def main():
+
+    animals = ["fox", "fly", "ant", "bee", "cat", "dog"]
+
+    print(animals[0], animals[1], animals[2], animals[3], animals[4], animals[5])
+
+
+main()
