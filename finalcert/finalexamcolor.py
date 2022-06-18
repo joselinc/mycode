44cb75e120e979818a22ebba0b8d3a7a5cf1e138
joselinc <joselinc@users.noreply.github.com>
2022-06-18 04:15:36 +0000

    Final commit for mycode folder

--- finalcert/finalexamcolor.py
+++ finalcert/finalexamcolor.py
@@ -6,10 +6,30 @@
 #import crayons package. Statement always goes on top before main code
 import crayons
 
-def main():
-    """run time code. Always indent under function"""
-    print("Hello, hello. Welcome to my python game. Can you guess the number I'm thinking?. You have 10 tries\n")
-    user_input = input("Enter the number: ")
+"""run time code. Always indent under function"""
+print(crayons.red(f"Hello, Welcome to my Python game.\nCan you guess the number I'm thinking?. You have 10 tries", bold=True))
 
-    for 
+def status():
+    
+    count = 10
+
+    while count > 0:
+        #ask user to input a number
+        user_input = int(input("Enter the number: "))
+        if user_input == 23:
+            print(crayons.green(f"YOU WIN", bold=True))
+            print(crayons.yellow(f"The game has ended. Goodbye!", bold=True))
+            break
+        
+        print(crayons.blue(f"Wrong answer. You have {count-1} attempts remaining....", bold=True))
+        count = count - 1 
+    else:
+        print(crayons.yellow(f"Sorry you lost the game :(", bold=True))
+
+def main():
+    status()
+main()
+
+
+
 
